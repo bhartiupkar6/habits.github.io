8e39ca20909d573ba6784fe4ac6795732f377b18
Upkar Bharti <52626463+bhartiupkar6@users.noreply.github.com>
2023-09-13 11:36:51 +0530

    Update db.js

--- app/db.js
+++ app/db.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 
 exports.connectMongoose =()=>{
     mongoose.set("strictQuery", false);
-    mongoose.connect('mongodb+srv://himadrinayak:12345@cluster0.h7n86ah.mongodb.net/csv-upload?retryWrites=true&w=majority', { useNewUrlParser: true })
+    mongoose.connect('mongodb+srv://bhartiupkar6:upkar6@cluster0.mhmtj8w.mongodb.net/cv-upload?retryWrites=true&w=majority', { useNewUrlParser: true })
     .then((e)=>console.log("Hi upkar connected to Mongodb => Habit-Tracker"))
     .catch((e)=>console.log("Hi upkar not connect Mongodb", e));
 }
